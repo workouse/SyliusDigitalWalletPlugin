b54d2beccabaf5b0f850b80e5a0308d62065590c
Ömer Büyükçelik <omerbuyukcelik@gmail.com>
2019-10-17 17:52:49 +0300

    updated: PromotionService

--- src/Service/PromotionService.php
+++ src/Service/PromotionService.php
@@ -5,6 +5,7 @@ namespace Workouse\DigitalWalletPlugin\Service;
 
 use Doctrine\ORM\EntityManager;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Sylius\Component\Customer\Model\Customer;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Sylius\Component\User\Canonicalizer\CanonicalizerInterface;
@@ -47,7 +48,23 @@ class PromotionService implements PromotionInterface
         $this->customerFactory = $customerFactory;
     }
 
-    function execute(Reference $reference)
+    public function referrerExecute(Reference $reference)
+    {
+        $customer = $this->customerFactory->createNew();
+        $customer->setEmail($reference->getReferrerEmail());
+        $customer->setEmailCanonical($this->canonicalizer->canonicalize($reference->getReferrerEmail()));
+        $this->entityManager->persist($customer);
+        $this->entityManager->flush();
+        $credit = new Credit();
+        $credit->setCustomer($this->customerRepository->findOneBy(['id' => $customer->getId()]));
+        $credit->setAction($this->referrer['action']);
+        $credit->setAmount($this->referrer['amount']);
+        $credit->setCurrencyCode($this->referrer['currency_code']);
+        $this->entityManager->persist($credit);
+        $this->entityManager->flush();
+    }
+
+    public function inviteeExecute(Reference $reference)
     {
         $credit = new Credit();
         $credit->setCustomer($reference->getInvitee());
@@ -55,20 +72,25 @@ class PromotionService implements PromotionInterface
         $credit->setAmount($this->invitee['amount']);
         $credit->setCurrencyCode($this->invitee['currency_code']);
         $this->entityManager->persist($credit);
-
-        $customer = $this->customerFactory->createNew();
-        $customer->setEmail($reference->getReferrerEmail());
-        $customer->setEmailCanonical($this->canonicalizer->canonicalize($reference->getReferrerEmail()));
-        $this->entityManager->persist($customer);
         $this->entityManager->flush();
+    }
 
-        $credit = new Credit();
-        $credit->setCustomer($this->customerRepository->findOneBy(['id' => $customer->getId()]));
-        $credit->setAction($this->referrer['action']);
-        $credit->setAmount($this->referrer['amount']);
-        $credit->setCurrencyCode($this->referrer['currency_code']);
-        $this->entityManager->persist($credit);
+    public function inviteeUserAfterExecute(Customer $customer)
+    {
+        $referrer = $this->entityManager->getRepository(Reference::class)->findOneBy([
+            'referrerEmail' => $customer->getEmail(),
+            'status' => false
+        ]);
 
-        $this->entityManager->flush();
+        if ($referrer) {
+            $credit = new Credit();
+            $credit->setCustomer($referrer->getInvitee());
+            $credit->setAction($this->invitee['action']);
+            $credit->setAmount($this->invitee['amount']);
+            $credit->setCurrencyCode($this->invitee['currency_code']);
+            $this->entityManager->persist($credit);
+            $referrer->setStatus(true);
+            $this->entityManager->flush();
+        }
     }
 }
